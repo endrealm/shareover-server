eac6a5076599ca628d82312af2b18a540bf69a08
CreepyPvP <interruptedThread@gmail.com>
2023-05-25 23:11:02 +0200

    Added user/me endpoint

--- src/UserController.ts
+++ src/UserController.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Put, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TypeOf, z } from "zod";
 import { prisma } from './const';
+import { v4 as uuidv4 } from 'uuid';
 
 const UserCreateData = z.object({
     username: z.string(),
@@ -14,15 +15,15 @@ type UserCreateData = z.infer<typeof UserCreateData>;
 export class UserController {
     constructor(private readonly appService: AppService) {}
 
-    @Get()
-    me(): string {
+    @Get("me")
+    me(@Req() req: Request): string {
         return "cool user";
     }
 
     @Put("create")
     async create(@Body() body: UserCreateData): Promise<string> {
         const data: UserCreateData = UserCreateData.parse(body); 
-        const token = "cooltoken"
+        const token = await this.generateToken();
         const user = await prisma.user.create({
             data:  {
                 name: data.username,
@@ -32,4 +33,13 @@ export class UserController {
         });
         return token;
     }
+
+    private async generateToken(): Promise<string> {
+        let token: string;
+        do {
+            token = uuidv4()
+        } while(!(await prisma.user.findUnique({where: {token}})))
+        return token;
+    }
+
 }
