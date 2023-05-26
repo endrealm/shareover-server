5597a883c95d9244110ee02f923aaf131e51e5f7
CreepyPvP <interruptedThread@gmail.com>
2023-05-26 16:45:18 +0200

    Added local env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from "./app.service";
 import { AuthMiddleware } from "./middleware/AuthMiddleware";
 import { GeoAPIService } from "./services/GeoAPIService";
 import { OfferController } from "./controllers/OfferController";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: [".env", ".env.local"],
+        }),
+    ],
     controllers: [UserController, OfferController],
     providers: [AppService, GeoAPIService],
 })
--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     // })
 
     const app = await NestFactory.create(AppModule);
-    // app.enableCors();
+    app.enableCors();
     await app.listen(3000);
 }
 
